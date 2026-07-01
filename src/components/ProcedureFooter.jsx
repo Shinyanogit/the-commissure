69fd00a3c8809b0b437d468c909d42d2f12d7974
imadarintaro <imadarintaro6@gmail.com>
2026-07-01 16:04:10 +0900

    added scene transition indicator

---
+++ src/components/ProcedureFooter.jsx
@@ -0,0 +1,14 @@
+export function ProcedureFooter() {
+    return (
+        <footer>
+            <div className='indicator'>
+                <span className="dot"></span>
+                <span className="dot active"></span>
+                <span className="dot"></span>
+                <span className="dot"></span>
+                <span className="dot"></span>
+            </div>
+            <div className="copyright">© 2026 The Commissure</div>
+        </footer>
+    );
+}
